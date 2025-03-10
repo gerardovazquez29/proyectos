9e6191a70c337e869a1939b9642c35e2485b95d4
Gerardo Vazquez <neowhatup@gmail.com>
2025-03-09 22:39:41 -0600

    lll

--- Recetas/mis_recetas.py
+++ Recetas/mis_recetas.py
@@ -36,22 +36,69 @@ def inicio():
         eleccion_menu = input()
     return (eleccion_menu)
 
-inicio()
+def mostrar_categorias(ruta):
+    print("categorias:")
+    ruta_categorias = Path(ruta)
+    lista_categorias = []
+    contador = 1
 
+
+    for carpeta in ruta_categorias.iterdir():
+        carpeta_str = str(carpeta.name)
+        print(f"[{contador}] - {carpeta_str}")
+        lista_categorias.append(carpeta)
+        contador += 1
+    
+    return lista_categorias
+
+def elegir_categoria(lista):
+    eleccion_correcta = 'x'
+
+    while not eleccion_correcta.isnumeric() or int(eleccion_correcta) not in range(1, len(lista) + 1):
+        eleccion_correcta = input("\n Elije una categoria: ")
+
+    return lista[int(eleccion_correcta) - 1]
+
+def mostrar_recetas(ruta):
+    print("Recetas: ")
+    ruta_recetas = Path(ruta)
+    lista_receta = []
+    contador = 1
+
+    for receta in ruta_recetas.glob('*.txt'):
+       receta_str = str(receta.name)
+       print(f"[{contador}] - {receta_str}")
+       lista_receta.append(receta)
+       contador += 1
+    return lista_receta 
+
+def elegir_recetas(lista):
+    eleccion_receta = 'x'
+
+    while not eleccion_receta.isnumeric() or int(eleccion_receta) not in range(1, len(lista) + 1):
+        eleccion_receta = input("\nElije una receta: ")
+    return lista[int(eleccion_receta) - 1]
+    
 
 menu = 0
 
 if menu == 1:
     # mostrar categorias
+    mis_categorias = mostrar_categorias(mi_ruta)
     # elegir categoria
+    mi_categoria = elegir_categoria(mis_categorias)
     # mostrar recetas
+    mis_recetas = mostrar_recetas(mi_categoria)
     # elegir recetas
+    mi_receta = elegir_recetas(mis_recetas)
     # leer receta
     # volver inicio
     pass
 elif menu == 2:
     # mostrar categorias
+    mis_categorias = mostrar_categorias(mi_ruta)
     # elegir categorias
+    mi_categoria = elegir_categoria(mis_categorias)
     # crear receta nueva
     # volver inicio
     pass
@@ -61,15 +108,21 @@ elif menu == 3:
     pass
 elif menu == 4:
     # mostrar categorias
+    mis_categorias = mostrar_categorias(mi_ruta)
     # elegir categoria
+    mi_categoria = elegir_categoria(mis_categorias)
     # mostrar recetas
+    mis_recetas = mostrar_recetas(mi_categoria)
     # elegir recetas
+    mi_receta = elegir_recetas(mis_recetas)
     # eliminar receta
     # volver inicio
     pass
 elif menu == 5:
     # mostrar categorias
+    mis_categorias = mostrar_categorias(mi_ruta)
     # elegir categoria
+    mi_categoria = elegir_categoria(mis_categorias)
     # eliminar categoria
     # volver inicio
     pass
