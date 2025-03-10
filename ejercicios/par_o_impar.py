255f9c36c56c144f9f51f6ebe3ae123cdc352825
Gerardo Vazquez <neowhatup@gmail.com>
2025-03-09 22:50:11 -0600

    ll

---
+++ ejercicios/par_o_impar.py
@@ -0,0 +1,11 @@
+
+# crea un programa que imprima True si el entero 
+# guardado en la variable num es par, si no imprima False
+
+num = 77
+
+resto = num % 2
+
+condicion = resto == 0
+
+print(condicion)
